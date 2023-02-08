c450f93a8d34cb6bd5f9eb40b7435dde3d81cf18
szjstar <73240804+szjstar@users.noreply.github.com>
2023-02-08 13:16:23 +0800

    built frameworks for field navigation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roboseed_AutoStage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roboseed_AutoStage.java
@@ -4,18 +4,21 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Robot.ComputerVisionFieldNavigation;
+
 @TeleOp(name = "AutoStateProgram_v1.0")
 public class Roboseed_AutoStage extends LinearOpMode {
     ElapsedTime elaspsedTime = new ElapsedTime();
 
+    ComputerVisionFieldNavigation fieldNavigation;
+
     @Override
     public void runOpMode() throws InterruptedException {
-
+        fieldNavigation = new ComputerVisionFieldNavigation(hardwareMap);
         waitForStart();
         elaspsedTime.reset();
         if (opModeIsActive()) {
             // find the top stick and place the goal
-
         }
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/ComputerVisionFieldNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/ComputerVisionFieldNavigation.java
@@ -1,4 +1,93 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XZY;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class ComputerVisionFieldNavigation {
+    private static final String VUFORIA_KEY = "AUrp8E//////AAABmR9EFFA6dECthZSIH4YjVNY8QqiA+WhfTqkO4koNt+QYbXrW9k3nqcvIL16tY13jQdTckP3BXh9+vKLJHQjATt6uffiXKfKUM3k+q3ZcB5g8v8+FT1WKJaOIf+vgOl84gghjBgira8FLQwuOB62wzkJRpoMKSNoyunOqG3PR7ttCl0izt9VRfaJy/2CmqLkXNj//tjcRj1xRqf3Xpo9omrSQcC8qQIRapKCcFUWyc+86WzQD2uVRwBAicmIs6yAHvUqMh/Xj0waomLYgvZ4CTDhgX2srIEgnLmlzxxIy2bggLV7vxnFILfe3wzLh1HYuQ0mHV9YF8ShgxXEbCrQDmUwripCsh3IST2Oxk/ZyVGZD";
+
+    private static final float mmPerInch        = 25.4f;
+    private static final float mmTargetHeight   = 6 * mmPerInch;          // the height of the center of the target image above the floor
+    private static final float halfField        = 72 * mmPerInch;
+    private static final float halfTile         = 12 * mmPerInch;
+    private static final float oneAndHalfTile   = 36 * mmPerInch;
+
+    private OpenGLMatrix lastLocation;
+    private VuforiaLocalizer vuforia;
+    private VuforiaTrackables targets;
+    private WebcamName webcamName;
+
+    private boolean targetVisible = false;
+
+    public ComputerVisionFieldNavigation (HardwareMap hardwareMap) {
+        webcamName = hardwareMap.get(WebcamName.class, "Webcam 1"); // get the instance of webcam
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = webcamName;
+        parameters.useExtendedTracking = false; // set the parameters for vuforia
+        vuforia = ClassFactory.getInstance().createVuforia(parameters); // create the vuforia instance
+
+        targets = this.vuforia.loadTrackablesFromAsset("PowerPlay"); // load the trained recognition model
+
+        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
+        allTrackables.addAll((Collection<? extends VuforiaTrackable>) targets); // put all the trackable into an array
+
+        // set the location of the navigation pictures in reference to the field
+        identifyTarget(0, "Red Audience Wall",   -halfField,  -oneAndHalfTile, mmTargetHeight, 90, 0,  90);
+        identifyTarget(1, "Red Rear Wall",        halfField,  -oneAndHalfTile, mmTargetHeight, 90, 0, -90);
+        identifyTarget(2, "Blue Audience Wall",  -halfField,   oneAndHalfTile, mmTargetHeight, 90, 0,  90);
+        identifyTarget(3, "Blue Rear Wall",       halfField,   oneAndHalfTile, mmTargetHeight, 90, 0, -90);
+
+        final float CAMERA_FORWARD_DISPLACEMENT  = 0.0f * mmPerInch;   // eg: Enter the forward distance from the center of the robot to the camera lens
+        final float CAMERA_VERTICAL_DISPLACEMENT = 6.0f * mmPerInch;   // eg: Camera is 6 Inches above ground
+        final float CAMERA_LEFT_DISPLACEMENT     = 0.0f * mmPerInch;   // eg: Enter the left distance from the center of the robot to the camera lens
+
+        OpenGLMatrix cameraLocationOnRobot = OpenGLMatrix
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XZY, DEGREES, 90, 90, 0));
+
+        for (VuforiaTrackable trackable : allTrackables) {
+            ((VuforiaTrackableDefaultListener) trackable.getListener()).setCameraLocationOnRobot(parameters.cameraName, cameraLocationOnRobot);
+        }
+    }
+
+    public double[] getRobotPosition () {
+
+    }
+
+    private void identifyTarget(int targetIndex, String targetName, float dx, float dy, float dz, float rx, float ry, float rz) {
+        VuforiaTrackable aTarget = targets.get(targetIndex);
+        aTarget.setName(targetName);
+        aTarget.setLocation(OpenGLMatrix.translation(dx, dy, dz)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, rx, ry, rz)));
+    }
+}
+
+class NavigationRunnable implements Runnable {
+    public NavigationRunnable () {
+        
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
