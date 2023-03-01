55101d08a21842184065c9e8a729cdeca70f6829
szjstar <73240804+szjstar@users.noreply.github.com>
2023-03-01 18:34:23 +0800

    continued improving the readability
    continued on explanations
    removed redundant codes
    added the hardware sketch in the documents

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/ArmControllingMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/ArmControllingMethods.java
@@ -43,7 +43,8 @@ public class ArmControllingMethods {
      * construct function of arm controlling methods
      * declare the hardware map and telemetry port, open the claw and relax the arm
      *
-     * @param HardwareDrive hr: the connection to the robot's hardware, Telemetry telemetry the port to remote console
+     * @param HardwareDrive hr: the connection to the robot's hardware
+     * @param Telemetry telemetry: the port to remote console
      * @return Nah
      * @throws Nah
      * */
@@ -55,6 +56,13 @@ public class ArmControllingMethods {
         deactivateArm();
     }
 
+    /*
+     * move the arm down into the following lower level
+     *
+     * @param Nah
+     * @return Nah
+     * @throws Nah
+     * */
     public void lowerArm() {
         System.out.println(arm);
         switch (arm) {
@@ -65,6 +73,13 @@ public class ArmControllingMethods {
         }
     }
 
+    /*
+     * move the arm up into the higher neighboured level
+     *
+     * @param Nah
+     * @return Nah
+     * @throws Nah
+     * */
     public void raiseArm() {
         switch (arm) {
             case -1: case 0: toLowArmPosition(); break;
@@ -73,35 +88,39 @@ public class ArmControllingMethods {
         }
     }
 
+    /*
+     * set the arm to match the higher, middle and lowest tower
+     *
+     * @param Nah
+     * @return Nah
+     * @throws Nah
+     * */
     public void toHighArmPosition() {
-        // highest position of the arm
+        /* highest position of the arm */
         int highPos = 700;
         elevateArm(highPos);
         arm = 3;
         telemetry.addData("going to top_pos", highPos);
         armStatus = true;
     }
-
     public void toMidArmPosition() {
-        // midpoint position of the arm
+        /* midpoint position of the arm */
         int midPos = 450;
         elevateArm(midPos);
         arm = 2;
         telemetry.addData("going to mid_pos", midPos);
         armStatus = true;
     }
-
     public void toLowArmPosition() {
-        // position of the arm when grabbing stuff
+        /* lower position of the arm */
         int lowPos = 280;
         elevateArm(lowPos);
         arm = 1;
         telemetry.addData("going to low_pos", lowPos);
         armStatus = true;
     }
-
     public void toGroundArmPosition() {
-        // lowest position of the arm
+        /* loading position of the arm */
         int gndPos = 45;
         elevateArm(gndPos);
         arm = 0;
@@ -109,20 +128,44 @@ public class ArmControllingMethods {
         armStatus = false;
     }
 
+    /*
+     * move the lowest position of the arm to load sleeves
+     *
+     * @param Nah
+     * @return Nah
+     * @throws Nah
+     * @Deprecated this method is longer suggested, use deactivateArm() instead
+     * */
+    @Deprecated
     public void toLoadingArmPosition() {
         // go to the arm position to load the sleeves
-        int loadingPos = 360; // TODO set this value precisely
+        int loadingPos = 360;
         elevateArm(loadingPos);
         arm = 1;
         armStatus = true;
     }
 
+    /*
+     * if the claw is opened, close it
+     * if the claw is closed, open it
+     *
+     * @param Nah
+     * @return Nah
+     * @throws Nah
+     * */
     public void open_closeClaw() {
         System.out.println("open_close");
         if(claw) {closeClaw(); return;}
         openClaw();
     }
 
+    /*
+     * open and close the claw
+     *
+     * @param Nah
+     * @return Nah
+     * @throws Nah
+     * */
     public void openClaw() {
         System.out.println("opening");
         claw = true;
@@ -130,7 +173,6 @@ public class ArmControllingMethods {
         // while (Math.abs(hr.claw.getPosition() - .35) > .05) Thread.yield(); // wait until the movement is finished, accept any inaccuracy below 5%
         armStatus = false;
     }
-
     public void closeClaw() {
         System.out.println("closing");
         claw = false;
@@ -139,7 +181,13 @@ public class ArmControllingMethods {
         armStatus = true;
     }
 
-
+    /*
+     * move the arm to the targeted position
+     *
+     * @param int position: the targeted position, ranged 0-1000, 0 is when the arm hits the robot badly, 1000 is when the arm flips around and damage the structer
+     * @return Nah
+     * @throws Nah
+     * */
     private void elevateArm(int position) {
         boolean isDecline = position < hr.lift_left.getCurrentPosition();
 
