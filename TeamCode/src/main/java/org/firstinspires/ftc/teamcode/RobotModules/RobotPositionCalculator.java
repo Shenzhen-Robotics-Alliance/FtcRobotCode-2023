f2189615ecc18b2fa0f79632c00f1569f1b89491
szjstar <73240804+szjstar@users.noreply.github.com>
2023-03-10 10:01:06 +0800

    edited TODO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/RobotPositionCalculator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/RobotPositionCalculator.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode.RobotModules;
 
+/**
+ * Copyright © 2023 SCCSC-Robotics-Club
+ * FileName: RobotPositionCalculator.java
+ *
+ * the module that calculates the robot's position according to the data from the encoders
+ *
+ * @Author 四只爱写代码の猫
+ * @Date 2023.2.27
+ * @Version v0.1.0
+ */
 public class RobotPositionCalculator {
+    // TODO write this method
 }
