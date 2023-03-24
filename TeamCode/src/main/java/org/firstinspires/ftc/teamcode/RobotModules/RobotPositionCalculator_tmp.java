89cc5a2abd2823dc8b1eb6f56a99743e49ffd037
szjstar <73240804+szjstar@users.noreply.github.com>
2023-03-24 23:02:43 +0800

    working on the simple position calculator...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/RobotPositionCalculator_tmp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/RobotPositionCalculator_tmp.java
@@ -23,8 +23,14 @@ import java.util.HashMap;
  * @Version v0.0.0
  */
 public class RobotPositionCalculator_tmp extends RobotModule {
-    /** the position scaling */
+    /** the module used to the read the data from encoders */
+    private Mini1024EncoderReader encoderReader;
 
+    /** some configurations of the robot TODO:measure these values */
+    /** the ratio between the angular velocity(in rad/s) to the difference in the velocity of the two parallel encoders (in encoder value) */
+    private final double angularVelocityPerParallelEncoderVelocityDifference = 1;
+    /** the ratio between the angular velocity(in rad/s) to the velocity of the third encoder, assuming that the robot's rotating center is still */
+    private final double angularVelocityPerThirdEncoderVelocity = 1;
 
     /**
      * construct method of temporary robot position calculator
@@ -33,9 +39,28 @@ public class RobotPositionCalculator_tmp extends RobotModule {
         super("temporaryPositionCalculator");
     }
 
+    /**
+     * initialize the temporary robot position calculator with given encoder reader module
+     *
+     * @param dependentModules the following modules are required for robot position calculator
+     *                         "encoderReader": Mini1024EncoderReader, the module that reads and processes the encoder value
+     * @param dependentInstances null should be given as this module does not dependent on any instance
+     */
     @Override
-    public void init(HashMap<String, RobotModule> dependentModules, HashMap<String, Object> dependentInstances) throws NullPointerException {
+    public void init(
+            HashMap<String, RobotModule> dependentModules,
+            HashMap<String, Object> dependentInstances
+    ) throws NullPointerException {
+        /* throw out an error if encoder reader module not given */
+        if (dependentInstances.isEmpty()) throw new NullPointerException (
+                "an empty set of dependent modules given to the module<<" + this.getModuleName() + ">> which requires at least one module(s) as dependency"
+        );
+        if (!dependentModules.containsKey("encoderReader")) throw new NullPointerException(
+                "dependency <<" + "encoderReader" + ">> not specified for module <<" + this.getModuleName() + ">>"
+        );
 
+        /* get the encoder reader module from the param */
+        this.encoderReader = (Mini1024EncoderReader) dependentModules.get("encoderReader");
     }
 
     @Override
