377fa9c9acc43fc30fc07e6d0558a3644c1eb6d5
szjstar <73240804+szjstar@users.noreply.github.com>
2023-05-18 15:02:31 +0800

    wrote a PID algorism for translation too

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/ChassisDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/ChassisDriver.java
@@ -15,9 +15,10 @@ public class ChassisDriver {
     private final double integralCoefficientRotation = 0; // not needed yet
 
     private final double maxMotioningPower = 0.5;
-    private final double cmStartDecelerate = 10;
-    private final double motorPowerPerTranslationDifference = (maxMotioningPower / cmStartDecelerate);
+    private final double encoderDifferenceStartDecelerate = 10;
+    private final double motorPowerPerEncoderDifference = (maxMotioningPower / encoderDifferenceStartDecelerate);
     private final double velocityDebugTimeTranslation = 0.1;
+    private final double integrationCoefficientTranslation = 0; // not needed yet
 
     private HardwareDriver hardwareDriver;
     private RobotPositionCalculator positionCalculator;
@@ -29,6 +30,7 @@ public class ChassisDriver {
     private double xAxleTranslationTarget = 0;
 
     private double yAxleTranslationTarget = 0;
+    private double[] translationalIntegration = new double[2];
     private double targetedRotation = 0;
     private boolean RASActivation = false;
 
@@ -36,13 +38,11 @@ public class ChassisDriver {
     private final int manualMode = 0;
     private int rotationMode = manualMode;
     private final int gotoPositionMode = 2;
-    private final int driverUsingTOFSensorMode = 3;
     private int translationalMode = manualMode;
 
-    private double integration;
+    private double rotationalIntegration;
 
     private ElapsedTime dt = new ElapsedTime();
-    private ElapsedTime dt1 = new ElapsedTime();
 
     public ChassisDriver(HardwareDriver hardwareDriver, RobotPositionCalculator positionCalculator) {
         this.hardwareDriver = hardwareDriver;
@@ -59,20 +59,22 @@ public class ChassisDriver {
         this.xAxleTranslationTarget = xAxleTranslation;
         this.yAxleTranslationTarget = yAxleTranslation;
         switchToGoToPositionMode();
+        translationalIntegration[0] = 0;
+        translationalIntegration[1] = 0;
     }
 
     /** in radian */
     public void setTargetedRotation(double targetedRotation) {
         this.targetedRotation = targetedRotation;
         switchToGoToPositionMode();
-        integration = 0;
+        rotationalIntegration = 0;
     }
 
     public void setRotationalMotion(double rotationalMotion) {
         switchToManualMode();
         this.rotationalMotion = rotationalMotion;
         dt.reset();
-        integration = 0;
+        rotationalIntegration = 0;
         sendCommandsToMotors();
     }
 
@@ -99,8 +101,8 @@ public class ChassisDriver {
     }
 
     public void sendCommandsToMotors(double tofDistanceSensorReading) {
-        if (rotationMode == goToRotationMode) updateRotationalMotorSpeed();
-        if (translationalMode == driverUsingTOFSensorMode) updateTranslationalMotionUsingTOFDistanceSensor(tofDistanceSensorReading);
+        if (rotationMode == goToRotationMode) updateRotationalMotorSpeed(dt.seconds());
+        if (translationalMode == gotoPositionMode) updateTranslationalMotionUsingEncoder(dt.seconds());
         hardwareDriver.leftFront.setPower(yAxleMotion + rotationalMotion + xAxleMotion);
         hardwareDriver.leftRear.setPower(yAxleMotion + rotationalMotion - xAxleMotion);
         hardwareDriver.rightFront.setPower(yAxleMotion - rotationalMotion - xAxleMotion);
@@ -108,7 +110,7 @@ public class ChassisDriver {
         // TODO make the robot stick to the rotation it was when pilot not sending commands on rotation
     }
 
-    private void updateRotationalMotorSpeed() {
+    private void updateRotationalMotorSpeed(double dt) {
         double currentRotation = positionCalculator.getRobotRotation();
         /* according to the angular velocity, predict the future rotation of the robot after velocity debug time */
         double futureRotation = currentRotation + velocityDebugTimeRotation * positionCalculator.getAngularVelocity();
@@ -116,22 +118,40 @@ public class ChassisDriver {
         double rotationalRawError = getActualDifference(currentRotation, targetedRotation);
         double rotationalError = getActualDifference(futureRotation, targetedRotation);
 
-        integration += rotationalRawError * dt.seconds();
+        rotationalIntegration += rotationalRawError * dt;
 
-        rotationalMotion = rotationalError * motorPowerPerRotationDifference + integration * integralCoefficientRotation;
+        rotationalMotion = rotationalError * motorPowerPerRotationDifference + rotationalIntegration * integralCoefficientRotation;
         rotationalMotion = Math.copySign(Math.min(maxRotatingPower, Math.abs(rotationalMotion)), rotationalMotion);
         // rotationalMotion *= -1;
         System.out.println("rotation:" + Math.toDegrees(positionCalculator.getRobotRotation()) + ";raw error:" + Math.toDegrees(rotationalRawError) + "; error:" + Math.toDegrees(rotationalError) + "; power" + rotationalMotion);
-        dt.reset();
     }
 
-    private void updateTranslationalMotionUsingTOFDistanceSensor(double tofDistanceSensorReading) {
-        double yAxlePosition = tofDistanceSensorReading;
-        double
+    private void updateTranslationalMotionUsingEncoder(double dt) {
+        double[] currentPosition = positionCalculator.getRobotPosition();
+        double currentRotation = positionCalculator.getRobotRotation();
 
-        double yAxlePositionPrediction = dt1.seconds() * ;
+        /* according to the robot's translational motion, predict it's future position */
+        double[] positionPrediction = new double[2];
+        positionPrediction[0] = positionCalculator.getVelocity()[0] * velocityDebugTimeTranslation + currentPosition[0];
+        positionPrediction[1] = positionCalculator.getVelocity()[1] * velocityDebugTimeTranslation + currentPosition[1];
 
-        dt1.reset();
+        double[] positionRawError = new double[]{xAxleTranslationTarget - currentPosition[0], yAxleTranslationTarget - currentPosition[1]};
+        double[] positionError = new double[] {xAxleTranslationTarget - positionPrediction[0], yAxleTranslationTarget- positionPrediction[1]};
+
+        translationalIntegration[0] += dt * positionRawError[0];
+        translationalIntegration[1] += dt * positionRawError[1];
+
+        xAxleMotion = positionError[0] * motorPowerPerEncoderDifference + translationalIntegration[0] * integrationCoefficientTranslation;
+        yAxleMotion = positionError[1] * motorPowerPerEncoderDifference + translationalIntegration[1] * integrationCoefficientTranslation;
+
+        xAxleMotion = Math.copySign(
+                Math.min(Math.abs(xAxleMotion), maxMotioningPower),
+                xAxleMotion
+        );
+        yAxleMotion = Math.copySign(
+                Math.min(Math.abs(xAxleMotion), maxMotioningPower),
+                yAxleMotion
+        );
     }
 
     public static double getActualDifference(double currentRotation, double targetedRotation) {
