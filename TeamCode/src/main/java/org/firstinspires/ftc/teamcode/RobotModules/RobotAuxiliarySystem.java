0e6e454d3b9cce3f857e8bcc58a86553afa8fe6b
szjstar <73240804+szjstar@users.noreply.github.com>
2023-05-26 23:36:02 +0800

    stable release: 90% successful rate tower aiming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/RobotAuxiliarySystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModules/RobotAuxiliarySystem.java
@@ -33,7 +33,7 @@ public class RobotAuxiliarySystem extends RobotModule {
     private static final double[] searchRangeList = {0, lowTowerSearchRange, midTowerSearchRange, highTowerSearchRange};
 
     /** the best dropping spot for the high tower, in cm */
-    private static final double highTowerDroppingSpot = 42;
+    private static final double highTowerDroppingSpot = 37.5;
     /** the best dropping spot for the mid tower, in cm */
     private static final double midTowerDroppingSpot = 36.5; // the arm is farther away when reaching for middle
     /** the best dropping spot for the low tower, in cm */
@@ -42,7 +42,7 @@ public class RobotAuxiliarySystem extends RobotModule {
     private static final double[] droppingSpotListEdge = {0, lowTowerDroppingSpot, midTowerDroppingSpot, 42.5};
 
     /** the angle between the sensor's aim center and the center of the arm, when approaching it from the left side */
-    private static final double aimCenterToDropCenterAngleLeft = Math.toRadians(-4);
+    private static final double aimCenterToDropCenterAngleLeft = Math.toRadians(-8);
     /** when approaching as rotating to the right */
     private static final double aimCenterToDropCenterAngleRight = Math.toRadians(6);
     /** when doing high-speed aim, which is to say, measure the rotation of one edge of the tower */
@@ -50,7 +50,7 @@ public class RobotAuxiliarySystem extends RobotModule {
     /** when approaching as rotating to the right at high speed*/
     private static final double aimEdgeToDropCenterAngleRight = Math.toRadians(0);
 
-    private static final double encoderValuePerCM = 6640 / 30; // measured that 6000 encoder values where increased for a 30cm of move
+    private static final double encoderValuePerCM = 6900 / 30; // measured that 6000 encoder values where increased for a 30cm of move
     private static final double encoderValuePerCMFastAim = 6540 / 30;
 
     private static final double positionCloseClaw = 0.25; // the distance, in color sensor distance unit, to the cone, for the robot to close its claw
@@ -471,7 +471,7 @@ public class RobotAuxiliarySystem extends RobotModule {
                     double distanceToDroppingSpot = (towerDistance - droppingSpotList[targetCode]) * encoderValuePerCM;
                     towerPosition[0] = positionCalculator.getRobotPosition()[0] + Math.cos(positionCalculator.getRobotRotation() + Math.toRadians(90)) * distanceToDroppingSpot;
                     towerPosition[1] = positionCalculator.getRobotPosition()[1] + Math.sin(positionCalculator.getRobotRotation() + Math.toRadians(90)) * distanceToDroppingSpot;
-                    System.out.println(Math.sin(positionCalculator.getRobotRotation() + Math.toRadians(90)) * distanceToDroppingSpot + "," + Math.cos(positionCalculator.getRobotRotation() + Math.toRadians(90)) * distanceToDroppingSpot);
+                    System.out.println("distance to dropping spot:" + distanceToDroppingSpot);
                     chassisDriver.setTargetedTranslation_fixedRotation(towerPosition[0], towerPosition[1]);
                     statusCode = 4;
                 }
